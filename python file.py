7a5ab10e0432d0c74c8ebf84b1993bbc388898f2
ilaylehchin <nika.fishman@gmail.com>
2025-09-07 15:07:22 +0300

    123456789

---
+++ python file.py
@@ -0,0 +1,44 @@
+# b=" "
+b=0
+a=("is country in the Southern Levant region of West Asia")
+# b=a.lower().replace("a-"  ,"ilay")
+for i in a:
+    if i=="a":
+        b+=1
+        print(b)
+
+
+
+
+# c=a.split("a")
+# print(c)
+# d=0
+# for i in c:
+#     if i=="a":
+
+
+# a=("ilay")
+# b=a.replace("a","g")
+# print(b)
+
+         # d=len(i)
+         # b=i
+         #
+# print(b)
+# print(d)
+
+# e=d
+# for i in c:
+#     if len (i)<e:
+#         e=len(i)
+#         b=i
+# print(e)
+# p
+
+
+
+
+# for i in a.lower():
+#     if i=="a":
+#         b+=1
+# print (b)
